72e2dca256e8f04be0837e0694c1df666372f90a
Jhoandris B <jrbaronp@gmail.com>
2024-08-31 12:37:33 -0500

    login clerk

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,14 @@
+export default function LayoutAuth({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className='flex  flex-col h-screen items-center justify-center '>
+      <p>Dashboard Companies</p>
+      <h1 className='text-3xl my-2'>Welcome</h1>
+      <h2 className='text-2xl mb-2'>Please sign in</h2>
+      {children}
+    </div>
+  )
+}
--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,5 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-  return (
-    <div className='flex min-h-screen flex-col items-center justify-center'>
-      <SignIn />
-    </div>
-  )
+  return <SignIn />
 }
